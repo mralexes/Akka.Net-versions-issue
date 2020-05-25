5e41bd2a8efbc8687fc9d2a114c3eb4e56e78801
Olexiy Kulchitskiy <olexiy.kulchitskiy@adraba.io>
2020-05-25 19:09:12 +0300

    Initial commit

---
+++ Akka_1_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Bootstrap.Docker;
+
+namespace Akka_1_4
+{
+    class Program
+    {
+        private static readonly string Config = @"
+        {
+            ""akka"": {
+              ""loglevel"": ""Warning"",
+              ""actor"": {
+                ""provider"": ""cluster"",
+              },
+                ""akka.remote.dot-netty.tcp"":{
+                ""batching"": {
+                ""enabled"": false
+                }
+              }
+            }
+          }
+        ";
+
+
+        static async Task Main(string[] args)
+        {
+            var bootstrapFromDocker = DockerBootstrap.BootstrapFromDocker(Config);
+
+            using (var actorSystem = ActorSystem.Create(Environment.GetEnvironmentVariable("ACTOR_SYSTEM"), bootstrapFromDocker))
+            {
+                await actorSystem.WhenTerminated;
+            }
+        }
+    }
+}
